07d8ebd179401e93c42bf70291f8541efc563c24
Derek <derekb0147@gmail.com>
2020-04-13 17:16:17 -0400

    Paged are in, they are not working

--- customuser/urls.py
+++ customuser/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('signup/', views.SignupView.as_view(), name='register'),
     path("login/", views.LoginView.as_view(), name="login"),
     path('logout/', views.logoutUser, name='logout'),
+    path('', views.main_view, name='main'),
+    path('buy/', views.buy_view, name='buy'),
+    path('company/', views.company_more_view, name='company'),
+    path('analysis/', views.fundamental_analysis_view, name='analysis')
 ]
+
+
--- customuser/views.py
+++ customuser/views.py
@@ -1,54 +1,10 @@
-# from .models import CustomUser
-# from .forms import CustomUserCreationForm, LoginForm
-# from django.contrib.auth import login, authenticate, logout
-# from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
-
-
-# def login_view(request):
-#     html = "login.html"
-#     if request.method == "POST":
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             user = authenticate(
-#                 email=data["email"],
-#                 password=data["password"])
-#             if user:
-#                 login(request, user)
-#                 return redirect(request.GET.get("next", "/"))
-#             else:
-#                 return HttpResponseRedirect("invalid authentication")
-#     form = LoginForm()
-#     return render(request, html, {'form': form})
-
-
-# def logout_view(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('login'))
-
-
-# def register_view(request):
-#     html = "genericForm.html"
-#     if request.method == "POST":
-#         form = CustomUserCreationForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             user = CustomUser.objects.create_user(
-#                 email=data["email"],
-#                 password=data["password1"]
-#             )
-#             login(request, user)
-#             return redirect(reverse('login'))
-#     form = CustomUserCreationForm()
-#     return render(request, html, {'form': form})
-
-
 from django.views import View
 from django.shortcuts import render, HttpResponseRedirect, redirect, reverse
 from django.contrib.auth import login, authenticate, logout
-# from django.contrib.auth.models import User
 from .forms import CustomUserCreationForm, LoginForm
 from customuser.models import CustomUser
+
+
 class LoginView(View):
     html = 'login.html'
     form_class = LoginForm
@@ -99,3 +55,28 @@ class SignupView(View):
 def logoutUser(request):
     logout(request)
     return HttpResponseRedirect(('/login/'))
+
+
+def main_view(request):
+    html = "main.html"
+    return render(request, html, {'context': "theContext"})
+
+
+def company_less_view(request):
+    html = "company_less.html"
+    return render(request, html, {'context': "theContext"})
+
+
+def company_more_view(request):
+    html = "company_more.html"
+    return render(request, html, {'context': "theContext"})
+
+
+def buy_view(request):
+    html = "buy.html"
+    return render(request, html, {'context': "theContext"})
+
+
+def fundamental_analysis_view(request):
+    html = "fundamental_analysis.html"
+    return render(request, html, {'context': "theContext"})
